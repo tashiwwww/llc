f219fe54815c023f8ef9edb5f76995ee66ae29e1
Peter <watashiwa@gmail.com>
2012-08-07 09:04:34 +0900

    it works!
    counts average px length of each line, compares fwf with vwf, gives
    number of extra chars per line.

--- llc.py
+++ llc.py
@@ -15,29 +15,25 @@ vwfstr = list()
 fwfstr	 = list()
 
 for line in script:
-	if(line[0] == '>'):
-		linecount += 1
-		realline = line[1:-1]
-		linelength = len(realline)
-		fwfpx = linelength*16
-		vwfpx = 0
-		#print(line)
-		for letter in realline:
-			try:
-				vwfpx += int(table[letter])
-			except KeyError:
-				vwfpx += 16 
-		allvwf.append(vwfpx)
-		allfwf.append(fwfpx)
-		vwfstr.append(vwfpx//(16*23))
-		fwfstr.append(fwfpx//(16*23))
+	linecount += 1
+	realline = line[1:-1]
+	linelength = len(realline)
+	fwfpx = linelength*16
+	vwfpx = 0
+	#print(line)
+	for letter in realline:
+		try:
+			vwfpx += int(table[letter])
+		except KeyError:
+			vwfpx += 16 
+	allvwf.append(vwfpx)
+	allfwf.append(fwfpx)
 
 fwflines = sum(allfwf)//linecount
 vwflines = sum(allvwf)//linecount
 
-print("Average str length with fixed-width:",fwflines)
-print("Average str length with variable-width:",vwflines)
+print(linecount,"total lines.")
+print("Average line length with fixed-width:",fwflines)
+print("Average line length with variable-width:",vwflines)
 print("Diff per string:",fwflines-vwflines, "px, or approximately",(fwflines-vwflines)//12, "extra letters")
-print("vwfstr is",sum(vwfstr)/len(vwfstr))
-print("fwfstr is",sum(fwfstr)/len(fwfstr))
 
